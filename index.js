ef7d696c089e25a9535c09f1d911edae64b4c1c2
Edwin Gomez <edwin.gomez@wolox.co>
2020-01-13 12:01:09 -0500

    Use trycatch global and try when errors

--- index.js
+++ index.js
@@ -21,34 +21,40 @@ puppeteer.launch({ headless: true }).then(async browser => {
   const page = (await browser.pages())[0]
   // infinite loop, use Cmd+C to stop
   while (true) {
-    await page.goto('https://www.speedtest.net/run')
-    await page.waitFor(5000);
-    console.log('Testing network. waiting for results...')
     try {
-      await page.waitForSelector('.js-start-test', { visible: true, timeout: WAIT_FOR_SELECTOR_TIMEOUT })
+      await page.goto('https://www.speedtest.net/run')
+      await page.waitFor(5000);
+      console.log('Testing network. waiting for results...')
+      try {
+        await page.waitForSelector('.js-start-test', { visible: true, timeout: WAIT_FOR_SELECTOR_TIMEOUT })
+      } catch(error) {
+        console.error(error);
+        await page.goto('https://www.google.com/')
+        console.info('Trying again...')
+        continue
+      }
+      const pingElement = await page.$(".ping-speed");
+      const downloadElement = await page.$(".download-speed");
+      const uploadElement = await page.$(".upload-speed");
+      const pingSpeed = await page.evaluate(element => element && element.textContent, pingElement);
+      const downloadSpeed = await page.evaluate(element => element && element.textContent, downloadElement);
+      const uploadSpeed = await page.evaluate(element => element && element.textContent, uploadElement);
+
+      if (pingSpeed && downloadSpeed && uploadSpeed) {
+        append({ pingSpeed, downloadSpeed, uploadSpeed })
+      } else {
+        await page.goto('https://www.google.com/')
+        console.info('Test results are null, trying again...')
+        continue
+      }
+
+      console.info(`Completed!! Check results file. Waiting ${WAIT_FOR_RETRY_TIMEOUT / 1000} seconds for try again`);
+      await page.waitFor(WAIT_FOR_RETRY_TIMEOUT);
     } catch(error) {
-      console.error(error);
-      await page.reload();
+      console.error(error)
+      await page.goto('https://www.google.com/')
       console.info('Trying again...')
-      continue
     }
-    const pingElement = await page.$(".ping-speed");
-    const downloadElement = await page.$(".download-speed");
-    const uploadElement = await page.$(".upload-speed");
-    const pingSpeed = await page.evaluate(element => element && element.textContent, pingElement);
-    const downloadSpeed = await page.evaluate(element => element && element.textContent, downloadElement);
-    const uploadSpeed = await page.evaluate(element => element && element.textContent, uploadElement);
-
-    if (pingSpeed && downloadSpeed && uploadSpeed) {
-      append({ pingSpeed, downloadSpeed, uploadSpeed });
-    } else {
-      await page.reload();
-      console.info('Trying again...')
-      continue
-    }
-
-    console.info(`Completed!! Check results file. Waiting ${WAIT_FOR_RETRY_TIMEOUT / 1000} seconds for try again`);
-    await page.waitFor(WAIT_FOR_RETRY_TIMEOUT);
   }
 });
 
